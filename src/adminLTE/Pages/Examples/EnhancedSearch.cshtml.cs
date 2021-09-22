7091a10734383135a6ed9599aeb437fbe7535ff1
skyrean <skyrean@gmail.com>
2021-09-22 20:23:51 +0900

    초기 커밋.

---
+++ src/adminLTE/Pages/Examples/EnhancedSearch.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace baseLine.templat.adminLTE.Pages.Examples
+{
+    public class EnhancedSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
